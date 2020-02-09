f4b45904fe1f5035a998843cf0e65b815c90cecc
kimiyazargari <kimiyazargari13@gmail.com>
2020-02-09 10:26:15 +0330

    fix function: mkTree; function: size, does not work.

--- randomTree.erl
+++ randomTree.erl
@@ -15,39 +15,40 @@
 -export([mkTree/1, size/1]).
 
 mkNode(Val, Parent) ->
-	{Val, Parent}.
+	{Val, Parent, []}.
 	
 	
 mkTree(List)->
 	Len = length(List),	
 	NewList = randomize(List, Len/2, Len),
 	Root = mkNode(hd(NewList), 'nil'),
-	mkTree(Root, tl(NewList)).
+	{Tree, _} = mkTree(Root, tl(NewList)),
+	if Tree == 'nil' -> Root;
+	true-> Tree
+	end.
+	
 	
 mkTree(Root, List) ->
 	Len = length(List),
 	NumOfChildren = rand:uniform(Len +1) - 1,
-	addChildren(Root, List, NumOfChildren, []).
-	
-	
-addChildren(Parent, List, Num, ChList) when Num > 0->	
-	addChildren(Parent, tl(List), Num -1, [mkNode(hd(List), Parent)|ChList]);
-	
-addChildren(Parent, List,_, ChList) ->
-	%{Val, P} = Parent,	
-	{NewChList, NewList} = mkTreeForEachChild(ChList, length(ChList), List, Parent, []),
-	NewParent = erlang : append_element(Parent, NewChList),
-	{NewParent, NewList}.
-	
-	
-mkTreeForEachChild(ChList,Len, List, Parent, NewChList)->
-	if (Len > 0) and (List =/= [])->
-		{NewChild, NewList} = mkTree(hd(ChList), List) ,
-		mkTreeForEachChild(tl(ChList), Len - 1, NewList, Parent, [NewChild|NewChList]);
-	true -> {NewChList, List} 
+	if NumOfChildren > 0 ->
+	addChildren(Root, List, NumOfChildren, [], 1,  NumOfChildren);
+	NumOfChildren =:= 0 ->
+	{'nil', List}
 	end.
-	 
-
+	
+		
+addChildren(Parent, List, Num, ChList,1, Num2) when (Num > 0) and (List =/= [])->	
+	 addChildren(Parent, tl(List), Num -1, [mkNode(hd(List), Parent)|ChList],1, Num2);
+		
+addChildren(Parent, List,_, ChList, Num3,  Num2) when (Num3 =< Num2) and (ChList =/= []) and (List =/= []) ->			
+	{CurrentChild, NewList} = mkTree(lists: nth(Num3, ChList), List),	
+	addChildren(Parent, NewList,0, [CurrentChild| tl(ChList)], Num3 + 1, Num2);	
+addChildren(Parent, List,_, ChList, _, _) ->
+	NewParent = {element(1, Parent), element(2, Parent), ChList},
+	{NewParent, List}.
+	
+	
 randomize(List, N, Len)->
 	if N >= 1 ->
 		LT = swap(List, rand: uniform(Len), rand: uniform(Len)),
@@ -56,27 +57,25 @@ randomize(List, N, Len)->
 	end.
 		
 swap(L, I, J)->
-	{L2,[F|L3]} = lists:split(I - 1 ,L),
+	{L2,[F|L3]} = lists:split(I - 1 ,L), 
 	L4 = L2 ++ [lists: nth( J, L)] ++ L3,
 	{L5, [_|L6]} = lists:split(J - 1,L4),
 	L5 ++  [F] ++ L6.
 	
 
-
 size(Tree)->
 	size(Tree, 1).
 size(Tree, Ans)->
-	{_,_, Children} = Tree,
+	{_,_,Children} = Tree,
 	CLen = length(Children),
 	if CLen =/= 0 ->
-		childIteration(Children, 1, Ans)
-	end,
-	Ans.
-	
-	
-childIteration(Children, N, Ans)->
-	Len = length(Children),
-	if N =< length(Children)->
-		size(lists: nth(N, Children), Ans + Len), 
-		childIteration(Children, N + 1,Ans + Len )
+		childIteration(Children, 1, Ans, CLen);
+	true -> Ans
 	end.
+	
+	
+childIteration(Children, N, Ans, Len) when (N =< Len)->
+		size(lists: nth(N, Children), Ans + Len), 
+		childIteration(Children, N + 1,Ans + Len, Len );
+childIteration(_, _, Ans, _) ->
+	Ans.
